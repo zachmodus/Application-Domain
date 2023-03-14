a608d8f21bcbb3cf22e2777a0d2ab7d96a6057b3
zachmodus <zacharyokwuosa@gmail.com>
2023-03-14 18:38:57 -0400

    Updates

---
+++ precache-manifest.1fa9fe2a6dd7b4204f158f84aef4aaf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "201b2d55706a2639a74c",
+    "url": "/AppyDomain/static/css/main.a2cdd059.chunk.css"
+  },
+  {
+    "revision": "201b2d55706a2639a74c",
+    "url": "/AppyDomain/static/js/main.201b2d55.chunk.js"
+  },
+  {
+    "revision": "03c018fb6e914630103e",
+    "url": "/AppyDomain/static/js/1.03c018fb.chunk.js"
+  },
+  {
+    "revision": "6a2cdaa1f50cc6ba5f7f",
+    "url": "/AppyDomain/static/js/2.6a2cdaa1.chunk.js"
+  },
+  {
+    "revision": "27f55647551b8444985d",
+    "url": "/AppyDomain/static/js/runtime~main.27f55647.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/AppyDomain/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "107762cdfd89b233d9249b44fd8e73c4",
+    "url": "/AppyDomain/index.html"
+  }
+];
