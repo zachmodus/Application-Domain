72d56846aff88d9ce0d932e8f2cf500055e60d15
mylesjoseph1 <92048320+mylesjoseph1@users.noreply.github.com>
2023-04-17 21:33:50 -0400

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -81,14 +81,14 @@ export const Register = (props) => {
 
 
     return (
-        <div className="auth-form-container">
+        <div className="login-form">
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
+            <label htmlFor="name">Full Name</label>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Full Name" />
+            <label htmlFor="email">Email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
+            <label htmlFor="password">Password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
             <button onClick={reggy}>Log In</button>
             <button onClick={CNU}>Create New User</button>
